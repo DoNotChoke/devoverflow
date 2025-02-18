e969efc26c3858a905513c419ee3c7f1de54518f
Whisper4 <nghiahh2004@gmail.com>
2025-02-18 11:56:48 +0700

    Set up Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const Home = () => (
-  <h1 className="background-light850_dark100 text-light-500">
-    Welcome to the world of Nextjs
-  </h1>
+  <>
+    <h1 className="h1-bold">Hello world</h1>
+    <h1 className="h1-bold font-inter">Hello world</h1>
+    <h1 className="h1-bold font-space-grotesk">Hello world</h1>
+  </>
 );
 
 export default Home;
