d144b0a2d88bf1d407bafcaea913f4397c4fc044
Whisper4 <nghiahh2004@gmail.com>
2025-02-19 15:42:46 +0700

    Set Up Auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-const Home = () => (
-  <>
-    <h1 className="h1-bold">Hello world</h1>
-    <h1 className="h1-bold font-inter">Hello world</h1>
-    <h1 className="h1-bold font-space-grotesk">Hello world</h1>
-  </>
-);
-
-export default Home;
