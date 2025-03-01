ce230a91dc7321277bec545798040fc33b6e1969
Whisper4 <nghiahh2004@gmail.com>
2025-03-02 00:13:33 +0700

    Implement get tags server action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 
-const Tags = () => {
+import { getTags } from "@/lib/actions/tag.action";
+
+const Tags = async () => {
+  const { success, data, error } = await getTags({
+    page: 1,
+    pageSize: 10,
+    query: "pytorch",
+  });
+  const { tags } = data || {};
+  console.log(JSON.stringify(tags, null, 2));
   return <div>Page</div>;
 };
 export default Tags;
